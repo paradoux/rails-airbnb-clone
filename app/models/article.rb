04ab2544d8d4c23a685ab60b86a786fac0606e10
Axel Martin <martin.axe@live.fr>
2017-08-18 16:42:40 +0200

    Added enum in Article and Order

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_many :orders
+
+  enum active: [:inactive, :active]
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :article
+
+  enum status: [:pendding, :confirmed, :to_prepare, :delivered, :cancelled]
 end
