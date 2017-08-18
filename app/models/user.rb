9279f67cf905f57065df96f150e6c6730d517a41
ChriGau <christelle.gaudron@laposte.net>
2017-08-18 18:58:36 +0200

    added user has_many to solve merging conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+         has_many :articles, dependent: :destroy
+         has_many :orders
+
 end
