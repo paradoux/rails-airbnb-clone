df596e93b944889f53acae29667511f9363acf80
Bruno Guers <bruno.guers@gmail.com>
2017-08-20 13:51:43 +0200

    Revert "comment several gems to figure out the problem"
    This reverts commit 9539f95a16ef0dffd29a8fb5dae2068d334da7e7.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
-
   config.action_mailer.default_url_options = { host: 'https://airbnb-paradoux.herokuapp.com/' }
   # Settings specified here will take precedence over those in config/application.rb.
 
