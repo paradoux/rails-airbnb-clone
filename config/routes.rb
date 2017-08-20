9539f95a16ef0dffd29a8fb5dae2068d334da7e7
Bruno Guers <bruno.guers@gmail.com>
2017-08-20 09:04:43 +0200

    comment several gems to figure out the problem

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  mount Attachinary::Engine => "/attachinary"
+  #mount Attachinary::Engine => "/attachinary"
 
   get'pages/concept'
 
