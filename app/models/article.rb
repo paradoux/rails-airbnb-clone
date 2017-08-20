3543a703afb3b346295886163542e6b327ba0631
Bruno Guers <bruno.guers@gmail.com>
2017-08-20 18:12:12 +0200

    qq validations, articles creation, edit, delete, index view updated, routes updated,

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,11 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :orders
+  has_many :orders, dependent: :destroy
+  validates :name, :quantity, :price, :unit, presence: true
 
   enum active: [:inactif, :actif]
+
+  def self.units
+    ["kg", "g", "unités", "l"]
+  end
 end
