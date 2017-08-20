30793d101a584a8a409d798f6964d5fab0605e79
Bruno Guers <bruno.guers@gmail.com>
2017-08-20 13:51:53 +0200

    Revert "marche toujours pas"
    This reverts commit 7b468b4ece3991eb06909517c4c1535eddeb6c50.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,9 +25,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
-  # BG test pour faire marcher heroku
-  #config.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
