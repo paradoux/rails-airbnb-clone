78b833f54ae58722d439e7f961f1625ee08ced7a
Axel Martin <martin.axe@live.fr>
2017-08-23 09:50:11 +0200

    Ajout Facebook connect

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170818124317) do
+ActiveRecord::Schema.define(version: 20170822162232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
-    t.string   "name"
     t.string   "unit"
     t.string   "description"
     t.string   "image"
@@ -27,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170818124317) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "active"
+    t.text     "name"
     t.index ["user_id"], name: "index_articles_on_user_id", using: :btree
   end
 
@@ -74,6 +74,11 @@ ActiveRecord::Schema.define(version: 20170818124317) do
     t.string   "password"
     t.string   "address"
     t.string   "bio"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "facebook_picture_url"
+    t.string   "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
